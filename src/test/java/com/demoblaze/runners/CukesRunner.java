2f901af8aca908ad2ddfed6e218c5fbfdf639c6c
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-02-06 15:06:46 +0000

    pom.xml has been fully setUp

---
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.demoblaze.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/demoblaze/step_definitions",
+        dryRun = true,
+        tags = "@wip"
+
+)
+public class CukesRunner {
+}
