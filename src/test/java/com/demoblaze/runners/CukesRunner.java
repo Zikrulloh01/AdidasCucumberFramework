7ad2bfb4f0897e2974bdbcee58e6883e9c46b97e
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-02-06 18:33:28 +0000

    All of the test steps have passed, Cucumber report has been generated

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@wip"
 
 )
